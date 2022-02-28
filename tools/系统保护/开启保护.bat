8c27818d36163c5c2d95bf8e5d9defac9d1a014b
SodaCris <18463922396@163.com>
2022-02-28 20:38:07 +0800

    更新系统保护

--- tools/系统保护/开启保护.bat
+++ tools/系统保护/开启保护.bat
@@ -3,4 +3,8 @@ fbwfmgr /addvolume c:
 FbwfMgr /addexclusion C: \Temp
 FbwfMgr /addexclusion C: \Users\user\AppData\Local\Temp
 fbwfmgr /addvolume D:
+fbwfmgr /addexclusion D: \TaiYuan-Release-v1.2.10\systemlog.txt
+fbwfmgr /addexclusion D: \程序信息记录.txt
+fbwfmgr /addexclusion D: \data
+fbwfmgr /addexclusion C: "\Program Files\PostgreSQL\14\data"
 shutdown /r /t 0
